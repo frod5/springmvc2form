0175bd91faea1ff91a0c1890fa2ca2b70a6709f4
sunwooKim <frod5@kakao.com>
2023-04-05 19:09:12 +0900

    spring mvc 2편 form - 코드 enum 및 class 추가 및 체크박스 실습. 체크박스는 value가 on으로 서버로 전송. 체크박스 미 선택시 값이 넘어가지 않아 스프링에선 _open 등으로 트릭으로 처리.

---
+++ src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -0,0 +1,17 @@
+package hello.itemservice.domain.item;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * FAST : 빠른배송
+ * NORMAL : 일반 배송
+ * SLOW : 느린 배송
+ */
+@Getter
+@RequiredArgsConstructor
+public class DeliveryCode {
+
+    private String code;
+    private String displayName;
+}
