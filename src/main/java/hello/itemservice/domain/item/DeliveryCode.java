09a029f18879465aa19f9a86bc15809ba2ed578e
sunwooKim <frod5@kakao.com>
2023-04-05 20:12:35 +0900

    spring mvc 2편 form - select box 실습

--- src/main/java/hello/itemservice/domain/item/DeliveryCode.java
+++ src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -1,5 +1,6 @@
 package hello.itemservice.domain.item;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,7 +10,7 @@ import lombok.RequiredArgsConstructor;
  * SLOW : 느린 배송
  */
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class DeliveryCode {
 
     private String code;
